f50b370befe306e56db355b080d2c13c62d76406
Alexey <alexey.petri4enko@gmail.com>
2015-06-25 23:17:53 +0300

    calculator 1.1

--- src/calc/calc.java
+++ src/calc/calc.java
@@ -11,9 +11,36 @@ public class calc {
         Scanner scanner = new Scanner(System.in);
         double num1 = scanner.nextDouble();
 
+
+
         System.out.println("Please input second number");
         double num2 = scanner.nextDouble();
+        if (num2 == 0){
+            System.out.println("Wrong digit! Please enter another");
+            System.out.println("Input second number");
+            num2 = scanner.nextDouble();
+        }
 
+        System.out.println("Please choose operation and input: + - * /");
+        String oper = scanner.next();
+        if (oper.equals("+")){
+            double sum = num1 + num2;
+            System.out.println("Sum = " + sum);
+        }else if(oper.equals("-")){
+            double diff = num1 - num2;
+            System.out.println("Difference = " + diff);
+        }else if(oper.equals("*")){
+            double mult = num1 * num2;
+            System.out.println("Multiplication = " + mult);
+        }else if(oper.equals("/")){
+            double div = num1 / num2;
+            System.out.println("Division = " + div);
+        }else{
+            System.out.println("You enter wrong symbol, please try again");
+        }
+
+
+       /*
         double sum = num1 + num2;
         System.out.println("Sum = " + sum);
         double diff = num1 - num2;
@@ -22,5 +49,6 @@ public class calc {
         System.out.println("Multiplication = " + mult);
         double div = num1 / num2;
         System.out.println("Division = " + div);
+        */
     }
 }
