59627a7e1a25d3c2f3e799fdd8968746fa34cd4d
Alexey <alexey.petri4enko@gmail.com>
2015-06-26 16:13:24 +0300

    rectangle 1.0

---
+++ src/graphicaStars/rectangle.java
@@ -0,0 +1,29 @@
+package graphicaStars;
+
+import java.util.Scanner;
+
+/**
+ * Created by Alexey on 6/26/15.
+ */
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the number of lines on the X axis");
+        int x = scanner.nextInt();
+        System.out.println("Please enter the number of lines on the Y axis");
+        int y = scanner.nextInt();
+
+        for (int i = 0;i <= y; i++) {
+                for (int j = 0; j <= x; j++) {
+                    if (j == 0 || j == x || i == 0 || i == y ) {
+                        System.out.print("* ");
+                    } else  System.out.print("  ");
+                }
+            System.out.print('\n');
+            }
+
+        }
+
+    }
+
