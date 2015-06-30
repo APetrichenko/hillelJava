2d1dc1d545d43017910a90197aca1b76f47ee107
Alexey <Alexey@Alexeys-Mac-mini.local>
2015-06-30 17:00:18 +0300

    студенты

---
+++ src/StudentsLastnames.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Alexey on 6/30/15.
+ */
+public class StudentsLastnames {
+    public static void main(String[] args) {
+
+       // String [] students = {"Smith","Johnson","Williams","White","Griffin","Adams","Scott","Black"};
+        String [] students = {"Smith","Johnson","Williams","White","Griffin","Adams","Scott","Black"};
+
+        System.out.println(students.length);
+
+        students = expandArray(students, 1);
+        students[8] = "New Student";
+
+        System.out.println(students[8]);
+        System.out.println(students.length);
+    }
+        public static String[] expandArray(String[] inArray, int expandBy) {
+        int initLength = inArray.length;
+        String[] result = new String[initLength+expandBy];
+        for (int i = 0; i <inArray.length;i++){
+            result[i] = inArray[i];
+        }
+        return result;
+    }
+
+}
+
