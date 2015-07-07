783f67226e617914cee2f0083948bb1412ed5fef
Alexey <Alexey@Alexeys-Mac-mini.local>
2015-07-07 18:30:50 +0300

    bla

---
+++ src/oop/NewArrayList.java
@@ -0,0 +1,14 @@
+package oop;
+
+/**
+ * Created by Alexey on 7/7/15.
+ */
+import java.util.Arrays;
+
+public class NewArrayList {
+    NewArrayList someList = new NewArrayList();
+    someList.add("Васька");
+
+}
+
+
