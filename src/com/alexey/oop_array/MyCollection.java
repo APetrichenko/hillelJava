71074a0080ece04ec38769ec595c16b7791c264d
APetrichenko <alexey.petri4enko@gmail.com>
2015-07-16 19:13:01 +0300

    collection

---
+++ src/com/alexey/oop_array/MyCollection.java
@@ -0,0 +1,57 @@
+package com.alexey.oop_array;
+
+import java.util.Collection;
+
+/**
+ * Created by Alexey on 7/14/15.
+ */
+public class MyCollection {
+    private Object[] list;
+    private int size;
+    public MyCollection(){
+        size = 0;
+        list = new Object[10];
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    public boolean add(Object object) {
+        return false;
+    }
+
+    public boolean remove(Object object) {
+        return false;
+    }
+
+    public boolean addAll(MyCollection collection) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean retainAll(MyCollection collection) {
+        return false;
+    }
+
+    public boolean removeAll(MyCollection collection) {
+        return false;
+    }
+
+    public boolean containsAll(MyCollection collection) {
+        return false;
+    }
+
+
+}
