80dfb545deddd6d48ab3e610aa4ee212b4d4844c
APetrichenko <alexey.petri4enko@gmail.com>
2015-07-17 15:33:51 +0300

    linked

---
+++ src/com/alexey/oop_linkedlist/MyCollection.java
@@ -0,0 +1,19 @@
+package com.alexey.oop_linkedlist;
+
+import sun.rmi.runtime.Log;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Alexey on 7/17/15.
+ */
+public class MyCollection {
+    List<String> linkedList = new LinkedList();
+    linkedList.add("Barsik");
+    linkedList.add("Murzik");
+    linkedList.add("Ryzhik");
+
+
+}
