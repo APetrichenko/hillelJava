a402230a850fba2f37103970660b54eecc618caf
Alexey <alexey.petri4enko@gmail.com>
2015-09-01 21:25:49 +0300

    builder

---
+++ src/com/alexey/patterns/builder/iter_1/App.java
@@ -0,0 +1,11 @@
+package com.alexey.patterns.builder.iter_1;
+
+/**
+ * Created by NewClass7 on 01.09.2015.
+ */
+public class App {
+    public void usageNutritionFacts(){
+        NutritionFacts cola = new NutritionFacts(240,8,100,0,35,27);
+        NutritionFacts burger = new NutritionFacts(200,1,540,45,12,46);
+    }
+}
